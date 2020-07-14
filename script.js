2410fb3f73c7b1d263e80d5a2b1c5c3326d43f2e
J D <jdiaztrinidad@csumb.edu>
2020-07-14 06:26:30 +0000

    Made formatting nicer

--- script.js
+++ script.js
@@ -11,30 +11,34 @@ function setup() {
   coinX = random(width);
   coinY = random(height);
   time = 1000;
-  score = 0
+  score = 0;
   gameIsOver = false;
-  fillrand1 = random(360)
-  fillrand2 = random(360)
-  timeDecrease = 0
-  ellipseScaling = windowHeight/25
-  startClicked = false
-  gameIsPlaying = false
+  fillrand1 = random(360);
+  fillrand2 = random(360);
+  timeDecrease = 0;
+  ellipseScaling = windowHeight / 25;
+  startClicked = false;
+  gameIsPlaying = false;
 }
 
 function draw() {
   //Background
   background(backgroundColor);
+
   //"Coins" that are meant to be collected
-  fill(fillrand1, 100, 100)
+  fill(fillrand1, 100, 100);
   ellipse(coinX, coinY, ellipseScaling);
+
   //Ellipse that follows cursor 
-  fill(fillrand2, 100, 100)
+  fill(fillrand2, 100, 100);
   ellipse(mouseX, mouseY, ellipseScaling);
+
   //Text that appears at the top left stating the time, score and if the game is over
-  fill(0, 0, 0)
+  fill(0, 0, 0);
   textAlign(LEFT)
   text(`Time remaining: ${time}`, 20, 40);
   text(`Score: ${score}`, 20, 20);
+
   //Functions that handle the countdown, collisions, start, and restart
   handleTime();
   handleCollision();
@@ -44,15 +48,17 @@ function draw() {
 
 function handleCollision() {
   
-  //Object collision detection using P5 collisions, I could have used this the other day tbh
+  //Object collision detection using P5 collisions
+  //I could have used this the other day tbh when making half pong
+
   hit = collideCircleCircle(mouseX, mouseY, ellipseScaling, coinX, coinY, ellipseScaling)
   if (hit == true && !gameIsOver && gameIsPlaying) {
 
-    score += 1
+    score += 1;
     coinX = random(width);
     coinY = random(height);
-    fillrand1 = random(360)
-   fillrand2 = random(360)
+    fillrand1 = random(360);
+   fillrand2 = random(360);
   }
 }
 
@@ -67,8 +73,8 @@ function handleTime() {
 
     gameIsOver = true;
     gameIsPlaying = false;
-    textAlign(LEFT)
-    text(`Game Over`, 20, 60)
+    textAlign(LEFT);
+    text(`Game Over`, 20, 60);
 
   }
 
@@ -77,24 +83,25 @@ function handleTime() {
 function startButton() {
 
   //Start Button
+
   if (!startClicked) {
-    rectMode(CENTER)
-    rect(windowWidthFixed/2, windowHeightFixed/2, windowWidthFixed/5, windowHeightFixed/20)
-    fill(0, 0, 100)
-    textAlign(CENTER)
-    text(`Start`, windowWidthFixed/2, windowHeightFixed/2)
+    rectMode(CENTER);
+    rect(windowWidthFixed / 2, windowHeightFixed / 2, windowWidthFixed / 5, windowHeightFixed / 20);
+    fill(0, 0, 100);
+    textAlign(CENTER);
+    text(`Start`, windowWidthFixed / 2, windowHeightFixed / 2);
 
   } else if (startClicked) {
 
-    timeDecrease = 1
+    timeDecrease = 1;
 
   } 
 
   //Start Button click detect
-  if (mouseY > windowHeightFixed/2 - windowHeightFixed/40 && mouseY < windowHeightFixed/2 + windowHeightFixed/40 && mouseX > windowWidthFixed/2 - windowWidthFixed/10 && mouseX < windowWidthFixed/2 + windowWidthFixed/10 && mouseIsPressed) {
+  if (mouseY > (windowHeightFixed / 2 - windowHeightFixed / 40) && mouseY < (windowHeightFixed / 2 + windowHeightFixed / 40) && mouseX > (windowWidthFixed / 2 - windowWidthFixed / 10) && mouseX < (windowWidthFixed / 2 + windowWidthFixed / 10) && mouseIsPressed) {
 
-    startClicked = true
-    gameIsPlaying = true
+    startClicked = true;
+    gameIsPlaying = true;
 
   }
 
@@ -104,20 +111,157 @@ function restartButton() {
 
   //Restart Button
   if (gameIsOver) {
-    rectMode(CENTER)
-    rect(windowWidthFixed/2, windowHeightFixed/2, windowWidthFixed/5,windowHeightFixed/20)
-    fill(0, 0, 100)
-    textAlign(CENTER)
-    text(`Restart`, windowWidthFixed/2, windowHeightFixed/2)
+    rectMode(CENTER);
+    rect(windowWidthFixed / 2, windowHeightFixed / 2, windowWidthFixed / 5,windowHeightFixed / 20);
+    fill(0, 0, 100);
+    textAlign(CENTER);
+    text(`Restart`, windowWidthFixed / 2, windowHeightFixed / 2);
   }
 
   //Restart Button click detection
-  if (mouseY > windowHeightFixed/2 - windowHeightFixed/40 && mouseY < windowHeightFixed/2 + windowHeightFixed/40 && mouseX > windowWidthFixed/2 - windowWidthFixed/10 && mouseX < windowWidthFixed/2 + windowWidthFixed/10 && mouseIsPressed) {
+  if (mouseY > (windowHeightFixed / 2 - windowHeightFixed / 40) && mouseY < (windowHeightFixed / 2 + windowHeightFixed / 40) && mouseX > (windowWidthFixed / 2 - windowWidthFixed / 10) && mouseX < (windowWidthFixed / 2 + windowWidthFixed / 10) && mouseIsPressed) {
 
-    gameIsOver = false
-    time = 1000
-    score = 0
+    gameIsOver = false;
+    time = 1000;
+    score = 0;
 
   }
 
 }
+
+
+
+
+
+
+// Dank's formatting revision with his linter
+
+// I've got to learn how to make my code look slightly cleaner
+// Also use ; more and not be a heathen
+
+
+
+// 'use strict';
+
+// let brushHue, backgroundColor, coinX, coinY, score, time, gameIsOver, hit, windowWidthFixed, windowHeightFixed, timeDecrease, startClicked, gameIsPlaying, fillrand1, fillrand2, ellipseScaling;
+
+// function setup() {
+
+//     // Canvas & color settings
+//     windowWidthFixed = windowWidth - 15;
+//     windowHeightFixed = windowHeight - 20;
+//     createCanvas(windowWidthFixed, windowHeightFixed);
+//     colorMode(HSB, 360, 100, 100);
+//     brushHue = 0;
+//     backgroundColor = 95;
+//     coinX = random(width);
+//     coinY = random(height);
+//     time = 1000;
+//     score = 0;
+//     gameIsOver = false;
+//     fillrand1 = random(360);
+//     fillrand2 = random(360);
+//     timeDecrease = 0;
+//     ellipseScaling = windowHeight / 25;
+//     startClicked = false;
+//     gameIsPlaying = false;
+// }
+
+// function draw() {
+
+//     // Background
+//     background(backgroundColor);
+
+//     // "Coins" that are meant to be collected
+//     fill(fillrand1, 100, 100);
+//     ellipse(coinX, coinY, ellipseScaling);
+
+//     // Ellipse that follows cursor
+//     fill(fillrand2, 100, 100);
+//     ellipse(mouseX, mouseY, ellipseScaling);
+
+//     // Text that appears at the top left stating the time, score and if the game is over
+//     fill(0, 0, 0);
+//     textAlign(LEFT);
+//     text(`Time remaining: ${time}`, 20, 40);
+//     text(`Score: ${score}`, 20, 20);
+
+//     // Functions that handle the countdown, collisions, start, and restart
+//     handleTime();
+//     handleCollision();
+//     startButton();
+//     restartButton();
+// }
+
+// function handleCollision() {
+
+//     // Object collision detection using P5 collisions, I could have used this the other day tbh
+//     hit = collideCircleCircle(mouseX, mouseY, ellipseScaling, coinX, coinY, ellipseScaling);
+//     if (hit === true && !gameIsOver && gameIsPlaying) {
+//         score += 1;
+//         coinX = random(width);
+//         coinY = random(height);
+//         fillrand1 = random(360);
+//         fillrand2 = random(360);
+//     }
+// }
+
+// function handleTime() {
+
+//     // Timer coundown
+//     if (time > 0) {
+
+//         time -= timeDecrease;
+
+//     } else {
+      
+//         gameIsOver = true;
+//         gameIsPlaying = false;
+//         textAlign(LEFT);
+//         text('Game Over', 20, 60);
+
+//     }
+
+// }
+
+// function startButton() {
+
+//     // Start Button
+//     if (!startClicked) {
+//         rectMode(CENTER);
+//         rect(windowWidthFixed / 2, windowHeightFixed / 2, windowWidthFixed / 5, windowHeightFixed / 20);
+//         fill(0, 0, 100);
+//         textAlign(CENTER);
+//         text('Start', windowWidthFixed / 2, windowHeightFixed / 2);
+
+//     } else if (startClicked) {
+//         timeDecrease = 1;
+//     }
+
+//     // Start Button click detect
+//     if (mouseY > (windowHeightFixed / 2) - (windowHeightFixed / 40) && mouseY < (windowHeightFixed / 2) + (windowHeightFixed / 40) && mouseX > (windowWidthFixed / 2) - (windowWidthFixed / 10) && (mouseX < windowWidthFixed / 2) + (windowWidthFixed / 10) && mouseIsPressed) {
+//         startClicked = true;
+//         gameIsPlaying = true;
+//     }
+
+// }
+
+// function restartButton() {
+
+//     // Restart Button
+//     if (gameIsOver) {
+//         rectMode(CENTER);
+//         rect(windowWidthFixed / 2, windowHeightFixed / 2, windowWidthFixed / 5, windowHeightFixed / 20);
+//         fill(0, 0, 100);
+//         textAlign(CENTER);
+//         text('Restart', windowWidthFixed / 2, windowHeightFixed / 2);
+//     }
+
+//     // Restart Button click detection
+//     if (mouseY > (windowHeightFixed / 2) - (windowHeightFixed / 40) && mouseY < (windowHeightFixed / 2) + (windowHeightFixed / 40) && mouseX > (windowWidthFixed / 2) - (windowWidthFixed / 10) && mouseX < (windowWidthFixed / 2) + (windowWidthFixed / 10) && mouseIsPressed) {
+//         gameIsOver = false;
+//         time = 1000;
+//         score = 0;
+//     }
+
+// }
